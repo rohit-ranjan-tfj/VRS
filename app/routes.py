e35384ad777bcf00a3d4ed89c500b7c9dd3a7575
thefurorjuror <ranjanmail.rohit@gmail.com>
2022-03-27 16:44:44 +0530

    Separate dashboards and landing page

--- app/routes.py
+++ app/routes.py
@@ -16,12 +16,9 @@ def before_request():
         db.session.commit()
 
 
-@app.route('/', methods=['GET', 'POST'])
-@app.route('/index', methods=['GET', 'POST'])
+@app.route('/index_user', methods=['GET', 'POST'])
 @login_required
-def index():
-    if request.form.get('Add Movie') == 'Add Movie':
-        return redirect(url_for('add_movie'))
+def index_user():
     page = request.args.get('page', 1, type=int)
     movies = Movie.query.order_by(Movie.timestamp.desc()).paginate(
         page, app.config['MOVIES_PER_PAGE'], False)
@@ -35,8 +32,26 @@ def index():
                            prev_url=prev_url)
 
 
-@app.route('/explore')
+@app.route('/index_staff', methods=['GET', 'POST'])
 @login_required
+def index_staff():
+    if request.form.get('Add Movie') == 'Add Movie':
+        return redirect(url_for('add_movie'))
+
+    return render_template('index.html', title='Home')
+
+
+@app.route('/index_manager', methods=['GET', 'POST'])
+@login_required
+def index_manager():
+    if request.form.get('Add Movie') == 'Add Movie':
+        return redirect(url_for('add_movie'))
+
+    return render_template('index.html', title='Home')                                                      
+
+
+@app.route('/')
+@app.route('/explore')
 def explore():
     page = request.args.get('page', 1, type=int)
     movies = Movie.query.order_by(Movie.timestamp.desc()).paginate(
@@ -51,8 +66,8 @@ def explore():
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
-    if current_user.is_authenticated:
-        return redirect(url_for('index'))
+    if current_user.is_authenticated :
+        return redirect(url_for('index'+"_" + current_user.user_cat))
     form = LoginForm()
     if form.validate_on_submit():
         cat = form.user_cat.data
@@ -63,7 +78,7 @@ def login():
         login_user(user, remember=form.remember_me.data)
         next_page = request.args.get('next')
         if not next_page or url_parse(next_page).netloc != '':
-            next_page = url_for('index')
+            next_page = url_for('index'+"_" + current_user.user_cat)
         return redirect(next_page)
     return render_template('login.html', title='Sign In', form=form)
 
@@ -71,13 +86,13 @@ def login():
 @app.route('/logout')
 def logout():
     logout_user()
-    return redirect(url_for('index'))
+    return redirect(url_for('explore'))
 
 
 @app.route('/register', methods=['GET', 'POST'])
 def register(user_cat = "user"):
     if current_user.is_authenticated:
-        return redirect(url_for('index'))
+        return redirect(url_for('index'+"_" + current_user.user_cat))
     form = RegistrationForm()
     if form.validate_on_submit():
 
@@ -93,7 +108,7 @@ def register(user_cat = "user"):
 @app.route('/staff_register', methods=['GET', 'POST'])
 def staff_register(user_cat = "staff"):
     if current_user.is_authenticated:
-        return redirect(url_for('index'))
+        return redirect(url_for('index'+"_" + current_user.user_cat))
     form = StaffRegistrationForm()
     if form.validate_on_submit():
         user = User(username=form.username.data, email=form.email.data,user_cat = user_cat)
@@ -108,7 +123,7 @@ def staff_register(user_cat = "staff"):
 @app.route('/reset_password_request', methods=['GET', 'POST'])
 def reset_password_request():
     if current_user.is_authenticated:
-        return redirect(url_for('index'))
+        return redirect(url_for('index'+"_" + current_user.user_cat))
     form = ResetPasswordRequestForm()
     if form.validate_on_submit():
         user = User.query.filter_by(email=form.email.data).first()
@@ -123,10 +138,10 @@ def reset_password_request():
 @app.route('/reset_password/<token>', methods=['GET', 'POST'])
 def reset_password(token):
     if current_user.is_authenticated:
-        return redirect(url_for('index'))
+        return redirect(url_for('index'+"_" + current_user.user_cat))
     user = User.verify_reset_password_token(token)
     if not user:
-        return redirect(url_for('index'))
+        return redirect(url_for('index'+"_" + current_user.user_cat))
     form = ResetPasswordForm()
     if form.validate_on_submit():
         user.set_password(form.password.data)
@@ -192,7 +207,7 @@ def follow(username):
         user = User.query.filter_by(username=username).first()
         if user is None:
             flash('User {} not found.'.format(username))
-            return redirect(url_for('index'))
+            return redirect(url_for('index'+"_" + current_user.user_cat))
         if user == current_user:
             flash('You cannot follow yourself!')
             return redirect(url_for('user', username=username))
@@ -201,7 +216,7 @@ def follow(username):
         flash('You are following {}!'.format(username))
         return redirect(url_for('user', username=username))
     else:
-        return redirect(url_for('index'))
+        return redirect(url_for('index'+"_" + current_user.user_cat))
 
 
 @app.route('/unfollow/<username>', methods=['POST'])
@@ -212,7 +227,7 @@ def unfollow(username):
         user = User.query.filter_by(username=username).first()
         if user is None:
             flash('User {} not found.'.format(username))
-            return redirect(url_for('index'))
+            return redirect(url_for('index'+"_" + current_user.user_cat))
         if user == current_user:
             flash('You cannot unfollow yourself!')
             return redirect(url_for('user', username=username))
@@ -221,13 +236,13 @@ def unfollow(username):
         flash('You are not following {}.'.format(username))
         return redirect(url_for('user', username=username))
     else:
-        return redirect(url_for('index'))
+        return redirect(url_for('index'+"_" + current_user.user_cat))
 
 @app.route('/add_movie', methods=['GET', 'POST'])
 @login_required
 def add_movie():
     if request.form.get('Back to Dashboard') == 'Back to Dashboard':
-        return redirect(url_for('index'))
+        return redirect(url_for('index'+"_" + current_user.user_cat))
     form = MovieForm()
     if form.validate_on_submit():
         movie = Movie(name=form.name.data,description = form.description.data, genre = form.genre.data,rating=form.rating.data,price = form.price.data,quantity=form.quantity.data, img_path=form.img_path.data)
