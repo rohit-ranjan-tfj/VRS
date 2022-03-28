929cba8166da1afb259de1ed8639d2e3415140c0
Vishal Raviapti <74860308+ElDestructo3@users.noreply.github.com>
2022-03-28 15:01:34 +0530

    Added audit and edited receipt generation.

--- app/functions.py
+++ app/functions.py
@@ -1,7 +1,9 @@
+from encodings import utf_8
 from app.models import User, Movie, Order
 from app import db
 from flask import flash
 from fpdf import FPDF
+from urllib.request import urlopen
 
 def rent_movie(user_id, movie_id, qty=1):
     try:
@@ -22,30 +24,40 @@ def rent_movie(user_id, movie_id, qty=1):
                 user_obj.balance -= movie_obj.price*qty
                 db.session.add(my_order)
                 db.session.commit()
-                flash('Congratulations. Movie Rented Successful')
+                flash('Congratulations. Movie Rented Successfully')
+                generate_receipt(my_order.id)
         else:
             raise ValueError("Movie Not Found!")
 
     except ValueError as e:
         flash(e)
 
-def generate_receipt(movie_id):
+def generate_receipt(order_id):
     try:
-        order_obj = Order.query.filter_by(id=movie_id).first()
+        order_obj = Order.query.filter_by(id=order_id).first()
+        movie_obj = Movie.query.filter_by(id=order_obj.movie_id).first()
+        user_obj = User.query.filter_by(id=order_obj.user_id).first()
 
-        if order_obj is not None:
+        if order_obj is not None and movie_obj is not None and user_obj is not None:
             receipt = FPDF()
             receipt.add_page()
             receipt.set_font('Arial', 'B', 16)
-            receipt.cell(200, 10, 'Movie Rentals', 0, 1, 'C')
+            receipt.cell(200, 10, 'VRS Movie Rentals', 0, 1, 'C')
+            receipt.cell(200, 10, 'Receipt', 0, 1, 'C')
             receipt.set_font('Arial', '', 12)
-            receipt.cell(200, 10, 'Order ID: {}'.format(order_obj.id), 0, 1, 'C')
-            receipt.cell(200, 10, txt=f"Movie ID: {order_obj.movie_id}", ln=1, align="C")
-            receipt.cell(200, 10, txt=f"Customer ID: {order_obj.user_id}", ln=1, align="C")
+            receipt.cell(200, 10, 'Order ID: {}'.format(order_obj.id), 0, 1, 'L')
+            receipt.cell(200, 10, txt=f"Customer ID: {order_obj.user_id}", ln=1, align="L")
+            receipt.cell(200, 10, txt=f"Customer Name: {user_obj.username}", ln=1, align="L")
+            receipt.cell(200, 10, txt=f"Movie ID: {order_obj.movie_id}", ln=1, align="L")   
+            receipt.cell(200, 10, txt=f"Movie Name: {movie_obj.name}", ln=1, align="L")
+            receipt.cell(200, 10, txt=f"Movie Genre: {movie_obj.genre}", ln=1, align="L")
+            receipt.cell(200, 10, txt=f" Date: {order_obj.timestamp.date()}", ln=1, align="L")
+            receipt.set_font('Arial', 'B', 16)
+            if order_obj.status=="YES":
+                receipt.cell(200, 10, txt=f"Status: Returned", ln=1, align="C")
+            else:
+                receipt.cell(200, 10, txt=f"Status: Not Returned", ln=1, align="C")
             receipt.cell(200, 10, txt=f"Total Price: {order_obj.price}", ln=1, align="C")
-            receipt.cell(200, 10, txt=f" Date: {order_obj.timestamp}", ln=1, align="C")
-            #receipt.cell(200, 10, txt=f"End Date: {order_obj.endDate}", ln=1, align="C")
-            receipt.cell(200, 10, txt=f"Order Status: {order_obj.status}", ln=1, align="C")
             receipt.output("receipt" + str(order_obj.id)+".pdf")
         
         else:
@@ -199,3 +211,30 @@ def search_movies(keyword):
             flash("\n")
     flash("Search complete.")
     
+def audit(user_id):
+    auditpdf = FPDF()
+    try:
+        user_obj = Order.query.filter_by(id=user_id).first()
+
+        if user_obj is not None:
+            if (user_obj.user_cat == 'staff'):
+                auditpdf.add_page()
+                auditpdf.set_font('Arial', 'B', 16)
+                auditpdf.cell(200, 10, 'Audit', 0, 1, 'C')
+                auditpdf.set_font('Arial', '', 12)
+
+              
+                total = 0
+                for order in Order.query.filter_by(user_cat='user'):
+                    auditpdf.cell(200, 10, 'Order ID: ' + str(order.id) + ', Price: '+ str(order.price), 0, 1, 'L')
+                    total += order.price
+                auditpdf.cell(200, 10, 'Total Money Collected:' + str(total), 0, 0, 'C')
+                
+                auditpdf.output("audit.pdf")
+            else:
+                flash('Applicable for staff only.')
+        else:
+            raise KeyError("Staff Not Found!")
+
+    except KeyError as e:
+        flash(e)
